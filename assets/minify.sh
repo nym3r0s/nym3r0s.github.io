8fdaf206338ef02ac7394d95f63fc6662d4a68db
Gokul Srinivas <gokusrinivas@gmail.com>
2016-08-28 18:31:18 +0530

    Major Overhaul

--- assets/minify.sh
+++
@@ -1,9 +0,0 @@
-cd ./css 
-cat *.css > ./min/style.css;
-cd  ./min;
-minify style.css;
-cd ./../../js;
-cat jquery.min.js > ./min/script.js;
-cat extra.js jquery.scrolly.min.js main.js skel.min.js typed.js util.js >> ./min/script.js
-cd ./min;
-minify script.js
